07f069081764f4a421de26809351a4f597cda38a
zth390872451 <zth390872451@gmail.com>
2016-11-02 09:58:13 +0800

    高可用云配置服务器 高可用注册中心 单一客户端 断路器

---
+++ config_server02/src/main/java/test/Application.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.UnknownHostException;
+
+/**
+
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String contextPath = environment.getProperty("server.context-path");
+        String serverPort = environment.getProperty("server.port");
+        log.info("Access URLs:127.0.0.1:{}:{}",contextPath,serverPort);
+     }
+}
